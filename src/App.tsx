96ccffa0ee292af656d03ddec3fd6ec20ff3ea52
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2024-07-18 07:43:03 -0300

    create components (ThereAreNoTasks & Task)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { PlusCircle } from 'phosphor-react';
 import './App.css';
 import { Header, Task } from './components';
 
@@ -6,8 +7,33 @@ export const App = () => {
 		<div>
 			<Header />
 			<div>
-				<main>
-					<Task content='teste' />
+				<main className='main-container'>
+					<div className='add-task-container'>
+						<input
+							type='text'
+							placeholder='Adicione uma nova tarefa'
+							className='add-task-field'
+						/>
+						<button type='submit' className='btn-create-task'>
+							Criar
+							<PlusCircle size={16} />
+						</button>
+					</div>
+					<div className='tasks-container'>
+						<div className='information-manager-container'>
+							<div className='managed-information'>
+								<span className='managed-information-text'>
+									Tarefas criadas
+								</span>
+								<span className='managed-information-count'>5</span>
+							</div>
+							<div className='managed-information'>
+								<span className='managed-information-text'>Concluídas</span>
+								<span className='managed-information-count'>2 de 5</span>
+							</div>
+						</div>
+						<Task content='teste' />
+					</div>
 				</main>
 			</div>
 		</div>
---
+++ src/components/ThereAreNoTasks/index.tsx
@@ -0,0 +1,9 @@
+export const ThereAreNoTasks = () => {
+	return (
+		<div>
+			<img src='/clipboard.svg' />
+			<p>Você ainda não tem tarefas cadastradas</p>
+			<p>Crie tarefas e organize seus itens a fazer</p>
+		</div>
+	);
+};
